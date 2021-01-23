b0eeaff321b08f6d0b6c1a8884c9a6f668476b6b
Gabriel <gabrielm.linassi@gmail.com>
2021-01-23 19:27:54 -0300

    Update Auth0.js

--- pages/api/utils/Auth0.js
+++ pages/api/utils/Auth0.js
@@ -6,9 +6,9 @@ export default initAuth0({
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
   scope: "openid profile",
   redirectUri:
-    "https://condescending-jennings-84e1f7.netlify.app/.netlify/functions/next_api_callback",
+    "https://main--condescending-jennings-84e1f7.netlify.app/api/callback",
   postLogoutRedirectUri:
-    "https://600c9c5adbb5fd0007174808--condescending-jennings-84e1f7.netlify.app/",
+    "https://main--condescending-jennings-84e1f7.netlify.app",
   session: {
     cookieSecret: process.env.COOKIE_SECRET,
   },
